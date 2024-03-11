d9f58b8e56b69408babe84cfa896b3bd2cd3955a
Sergey Videtskikh <videtskikh@gmail.com>
2024-03-11 16:20:23 +0300

    init project

---
+++ app/src/main/java/ru/vsv/retrofitmoviessearchtask/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.vsv.retrofitmoviessearchtask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
